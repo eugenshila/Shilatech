70c3307a66eee5787c381c73c0d8986cc667bdb0
eugenshila <eugenshila@gmail.com>
2026-08-20 22:20:43 +0300

    Load warehouse portal styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import '../styles/home-premium.css';
+import '../styles/warehouse.css';
 import { CartProvider } from '../components/CartContext';
 
 export default function App({ Component, pageProps }) {
