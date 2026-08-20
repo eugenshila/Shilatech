7550f3ce997f2fc25680a1c4c532ea2cd82be5f2
eugenshila <eugenshila@gmail.com>
2026-08-20 21:26:21 +0300

    Load premium homepage styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/home-premium.css';
 import { CartProvider } from '../components/CartContext';
 
 export default function App({ Component, pageProps }) {
