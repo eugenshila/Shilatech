e301dbf7e38c7f7870494a1428d11af0f69e7afc
eugenshila <eugenshila@gmail.com>
2026-08-20 22:56:29 +0300

    Load delivery PDA styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import '../styles/home-premium.css';
 import '../styles/warehouse.css';
 import '../styles/warehouse-premium.css';
+import '../styles/delivery.css';
 import { CartProvider } from '../components/CartContext';
 
 export default function App({ Component, pageProps }) {
