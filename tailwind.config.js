97841eae2493375badcc3a8ac3fd2c638912e523
eugenshila <eugenshila@gmail.com>
2026-08-20 16:43:56 +0300

    Shift Shilatech palette to graphite blue and silver

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        shilaBlack: '#050505',
-        shilaCharcoal: '#111111',
-        shilaCard: '#151515',
-        shilaSilver: '#b8b8b8',
-        shilaGold: '#d6a928',
-        shilaGoldLight: '#f0c94c',
-        shilaAccent: '#d6a928'
+        shilaBlack: '#0b0f14',
+        shilaCharcoal: '#111821',
+        shilaCard: '#151d27',
+        shilaSilver: '#b9c4d0',
+        shilaGold: '#168bff',
+        shilaGoldLight: '#67b7ff',
+        shilaAccent: '#168bff'
       },
       boxShadow: {
-        gold: '0 0 0 1px rgba(214,169,40,.35), 0 18px 45px rgba(0,0,0,.45)'
+        gold: '0 0 0 1px rgba(22,139,255,.30), 0 18px 45px rgba(0,0,0,.42)'
       }
     }
   },
