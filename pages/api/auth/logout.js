d2c9fe5e8d747a20fe2bfd4b15d4590bf9f6bf79
eugenshila <eugenshila@gmail.com>
2026-08-20 20:03:22 +0300

    Add logout API

---
+++ pages/api/auth/logout.js
@@ -0,0 +1,7 @@
+import { clearSession } from '../../../lib/auth';
+
+export default function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
+  clearSession(res);
+  res.status(200).json({ ok: true });
+}
