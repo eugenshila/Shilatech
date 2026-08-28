9ad9c8a9b121a934e9f19d11dacc8245a32aa8f7
eugenshila <eugenshila@gmail.com>
2026-08-28 18:54:25 +0300

    Apply approved Lora font throughout website with self-hosted Next font

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Lora } from 'next/font/google';
 import '../styles/globals.css';
 import '../styles/home-premium.css';
 import '../styles/warehouse.css';
@@ -11,11 +12,18 @@ import { CartProvider } from '../components/CartContext';
 import StaffBoundary from '../components/StaffBoundary';
 import '../styles/staff.css';
 
+const lora = Lora({ subsets: ['latin'], display: 'swap', preload: true });
+
 export default function App({ Component, pageProps }) {
   return (
+    <>
+      <style jsx global>{`
+        html, body { font-family: ${lora.style.fontFamily}; }
+      `}</style>
     <CartProvider>
       <StaffBoundary><Component {...pageProps} /></StaffBoundary>
     </CartProvider>
+    </>
   );
 }
 
