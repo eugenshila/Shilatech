a082123c65073aba8786ec76e850aaec2cf87e3d
eugenshila <eugenshila@gmail.com>
2026-08-29 00:24:58 +0300

    Centre and enlarge staff workspace logo watermarks

--- components/StaffBrandBackground.js
+++ components/StaffBrandBackground.js
@@ -5,12 +5,12 @@ export default function StaffBrandBackground(){
    background-color:#080d08;
    background-image:linear-gradient(90deg,rgba(8,13,8,.94) 0%,rgba(8,13,8,.80) 60%,rgba(8,13,8,.73) 100%),url('/shilatech-logo.webp');
    background-repeat:no-repeat;
-   background-size:cover,560px auto;
-   background-position:center,right 32px top 50px;
+   background-size:cover,680px auto;
+   background-position:center,center;
   }
   #employee-dashboard{min-height:65vh;box-sizing:border-box}
-  #employee-dashboard:has(.backLink){min-height:0;background-size:cover,260px auto;background-position:center,right 30px center}
-  div.functionView{min-height:72vh;background-position:center,right 32px top 80px}
+  #employee-dashboard:has(.backLink){min-height:0;background-size:cover,320px auto;background-position:center,center}
+  div.functionView{min-height:72vh;background-position:center,center}
   .functionView .counterShell,.functionView .hrPage,.functionView .warehousePage,.functionView .deliveryPage,.functionView .deliveryShell{
    background-color:transparent!important;
   }
@@ -18,8 +18,8 @@ export default function StaffBrandBackground(){
    background-color:rgba(8,15,8,.94);
   }
   @media(max-width:640px){
-   #employee-dashboard,div.functionView{background-size:cover,340px auto;background-position:center,right -80px top 100px}
-   #employee-dashboard:has(.backLink){background-size:cover,200px auto;background-position:center,right -50px center}
+   #employee-dashboard,div.functionView{background-size:cover,min(420px,90vw) auto;background-position:center,center}
+   #employee-dashboard:has(.backLink){background-size:cover,240px auto;background-position:center,center}
   }
   @media print{
    #employee-dashboard,div.functionView,.functionView .counterShell,.functionView .hrPage{background-image:none!important;background-color:white!important}
